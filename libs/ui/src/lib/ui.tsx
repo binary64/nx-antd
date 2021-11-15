5da076b41f53c87224a981621ff112958d60d344
binary64 <1680627+binary64@users.noreply.github.com>
2021-11-15 13:45:35 +0000

    added svg

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -2,6 +2,7 @@ import { Button } from 'antd';
 import styles from './ui.module.less'
 import testImg from './assets/test.png'
 import { Img } from './img/img';
+import { ReactComponent as SvgImage } from './assets/acne.svg'
 
 /* eslint-disable-next-line */
 export interface UiProps {}
@@ -16,6 +17,7 @@ export function Ui(props: UiProps) {
     <div className={styles['kebab-case']}>local kebabcase but jsx square brackets</div>
     <Button type={'primary'} className={styles.bigAntButton}>antd button</Button>
     <div className={styles.bigAntButton2}><Button type={'primary'}>antd button</Button></div>
+    <p>SVG:<SvgImage/></p>
   </>
 }
 
